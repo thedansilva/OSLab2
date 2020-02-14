1a6e75736440f31f38a02ac7711923647fa2a2cd
thedansilva <43654534+thedansilva@users.noreply.github.com>
2020-02-14 12:58:26 -0500

    cd functionality

--- shell.c
+++ shell.c
@@ -7,24 +7,45 @@
 #define clear() printf("\033[H\033[J")
 // #define gotoxy(x, y) printf("\033[%d;%dH", y, x)
 
-int main(){
+int main(int argc, char *argv[]){
 	//clear() already works
-	
 	char str[255];
 	char command[5];
+	char cwd[100];
+	char args[256][256];
+	int argCount = 0;
 	char *token;
-	printf("Type some stuff: ");
-	fgets(str, 255, stdin);
-	token = strtok(str, " ");
-	strcpy(command, token);
-	printf("Command is %s\n", command);
-	token = strtok(NULL, " ");
-	while(token != NULL) {
-		printf("%s\n", token);
-		token = strtok(NULL, " ");
+	while (1) 
+	{
+	if (getcwd(cwd, sizeof(cwd)) != NULL) {
 	}
-        if (strcmp(command, "cd") == 0 || strcmp(command, "cd\n") == 0){
-		printcwd();
+	printf("%s$ ", cwd);
+	fgets(str, 255, stdin);
+	str[strcspn(str, "\n")] = 0; //remove trailing new line from string before tokenizing
+	token = strtok(str, " ");
+	argCount = 0;
+	strcpy(command, token);
+	while(token != NULL) {
+		token = strtok(NULL, " ");
+		if (token != NULL) {
+			//printf("Current token is %s\n", token);
+			strcpy(args[argCount], token);
+			argCount++;
+		}
+
+	}
+	if (strcmp(command, "cd") == 0 || strcmp(command, "cd\n") == 0) {
+		if(strlen(args[0]) <= 0) { //if there's no input past 'cd'
+			printcwd();
+		}
+		else {
+			if (chdir(args[0]) == 0) 
+			{
+			} else printf("Directory %s not found\n", args[0]);
+		}
+	}
+	memset(args, 0, sizeof(args));
+	memset(str, 0, 255);
 	}
     	//showsomething();
 }
