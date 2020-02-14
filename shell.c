2616f46651c090191aae08b181a26543e0258a7c
thedansilva <43654534+thedansilva@users.noreply.github.com>
2020-02-14 18:35:21 -0500

    gogurt

--- shell.c
+++ shell.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <dirent.h>
 #include "shell.h"
 
 #define clear() printf("\033[H\033[J")
 // #define gotoxy(x, y) printf("\033[%d;%dH", y, x)
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[], char **envp){
 	//clear() already works
 	char str[255];
 	char command[5];
@@ -17,7 +18,8 @@ int main(int argc, char *argv[]){
 	char *token;
 	while (1) 
 	{
-	if (getcwd(cwd, sizeof(cwd)) != NULL) {
+	if (getcwd(cwd, sizeof(cwd)) != NULL) 
+	{
 	}
 	printf("%s$ ", cwd);
 	fgets(str, 255, stdin);
@@ -25,16 +27,19 @@ int main(int argc, char *argv[]){
 	token = strtok(str, " ");
 	argCount = 0;
 	strcpy(command, token);
-	while(token != NULL) {
+	while(token != NULL) 
+	{
 		token = strtok(NULL, " ");
-		if (token != NULL) {
+		if (token != NULL) 
+		{
 			//printf("Current token is %s\n", token);
 			strcpy(args[argCount], token);
 			argCount++;
 		}
 
 	}
-	if (strcmp(command, "cd") == 0 || strcmp(command, "cd\n") == 0) {
+	if (strcmp(command, "cd") == 0 || strcmp(command, "cd\n") == 0) 
+	{
 		if(strlen(args[0]) <= 0) { //if there's no input past 'cd'
 			printcwd();
 		}
@@ -44,21 +49,73 @@ int main(int argc, char *argv[]){
 			} else printf("Directory %s not found\n", args[0]);
 		}
 	}
+	else if (strcmp(command, "cls") == 0 || strcmp(command, "cls\n") == 0)
+	{
+		clear();
+	}
+	else if (strcmp(command, "dir") == 0 || strcmp(command, "dir\n") == 0)
+	{
+		DIR *dir;
+		struct dirent *dirent;
+		if (strlen(args[0]) <= 0) 
+		{
+		dir = opendir(".");
+		}
+		else
+		{
+		dir = opendir(args[0]);
+		}
+		if (dir) 
+		{
+			while ((dirent = readdir(dir)) != NULL)
+				{
+				printf("%s\n", dirent->d_name);
+				}
+			closedir(dir);
+		}	
+	}
+	else if (strcmp(command, "environ") == 0 || strcmp(command, "environ\n") == 0)
+	{
+		for (char **env = envp; *env != 0; env++)
+		{
+			char *thisEnv = *env;
+			printf("%s\n", thisEnv);    
+		}
+	}
+	else if (strcmp(command, "echo") == 0 || strcmp(command, "echo\n") == 0)
+	{
+		for(int i = 0; i < argCount; i++) {
+			printf("%s ", args[i]);
+		}
+		printf("\n");
+	}
+	else if (strcmp(command, "help") == 0 || strcmp(command, "pause\n") == 0)
+	{
+		help();
+	}
+	else if (strcmp(command, "pause") == 0 || strcmp(command, "pause\n") == 0)
+	{
+		char resumer[256];
+		printf("Pausing shell until Enter is pressed.\n");
+		fgets(resumer, sizeof(resumer), stdin);
+		do 
+		{
+			fgets(resumer, sizeof(resumer), stdin);
+		} while (strcmp(resumer, "\n") != 0); 
+		printf("Resuming.\n");
+		
+	}
+	else if (strcmp(command, "quit") == 0 || strcmp(command, "quit\n") == 0)
+	{
+		return 0;
+	}
+	else
+	{
+		printf("***\nUnknown command. Invoking %s on native bash.\n", command);
+		system(command);
+		printf("***\n");
+	}
 	memset(args, 0, sizeof(args));
 	memset(str, 0, 255);
-	}
-    	//showsomething();
-}
-
-void print_env_vars() {
-    // const char* s = getenv("USER");
-    const char* s = getenv("PATH");
-    printf("%s\n", (s != NULL) ? s : "");
-}
-
-void printcwd() {
-	char cwd[100];
-	if(getcwd(cwd, sizeof(cwd)) != NULL){
-		printf("%s\n", cwd);
 	}
 }
--- shell.h
+++ shell.h
@@ -1,7 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-void printcwd();
-void print_env_vars();
 void showsomething(){
     printf("Hmm it showed something\n");
+}
+void print_env_vars() {
+    // const char* s = getenv("USER");
+    const char* s = getenv("PATH");
+    printf("%s\n", (s != NULL) ? s : "");
+}
+
+void printcwd() {
+	char cwd[100];
+	if(getcwd(cwd, sizeof(cwd)) != NULL){
+		printf("%s\n", cwd);
+	}
+}
+
+void help() {
+	printf("*****\ncd <directory>: change directory (or leave blank to see cwd)\nclr: clear screen\ndir <directory>: list contents of directory\nenviron: list environment strings\necho <comment>: display a comment\nhelp: display this help\npause: pause the shell until enter is pressed\nquit: quit the shell\nAdditionally, you may execute any native bash shell function outside\nof these, but they are not from this shell itself.\n*****\n");
 }
